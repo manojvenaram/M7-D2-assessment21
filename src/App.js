8d5f396259a89a1c17b0faa8f7e55a04621f4219
MANOJ CHOUDHARY V <manojsasi20032005@gmail.com>
2024-07-09 15:18:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import logo1 from './logo1.svg';
 import './App.css';
 
 function App() {
+  let n=0;
+  const handleDetail=()=>{
+    
+    if (n%2==0){
+      document.getElementById('id2').innerHTML="HideDetails"
+      document.getElementById('id1').innerHTML="Additional details or content can go here..."
+    }
+    else{
+      document.getElementById('id2').innerHTML="ShowDetails"
+
+    document.getElementById("id1").innerHTML=""
+    
+    }
+    n++;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <img src={logo1} className="App-logo" alt="logo" />
+        <p className="id4" ><strong>NAME : MANOJ V </strong><br></br></p>
+        <p className="id3">RegisterNumber:212221240025</p>
+        <button className="button" onClick={handleDetail} id="id2">Show Details</button>
+        <br></br>
+        <div id="id1"></div>
+        </div>
+        
     </div>
   );
 }
